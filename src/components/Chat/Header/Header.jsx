2f1ee4d663434b545fbbaeb3e5fa389cb37ccb6f
Aamir Khan <aamir.fastnu@gmail.com>
2018-07-30 21:41:22 +0500

    Use Dropdown component

--- src/components/Chat/Header/Header.jsx
+++ src/components/Chat/Header/Header.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
+import Dropdown from '../../Dropdown/Dropdown';
+
 import pp from '../../../assets/pp.jpg';
 
 import './Header.scss';
 
 const header = () => (
-    <header className="chat-header">
-        <img className='thumbnail' src={pp} />
+  <header className="chat-header">
+    <img className="thumbnail" src={pp} />
 
-        <div>
-            <i className="fa fa-search" />
-            <i className="fa fa-paperclip" />
-            <i className="fa fa-ellipsis-h" />
-        </div>
-    </header>
+    <div>
+      <i className="fa fa-search" />
+      <i className="fa fa-paperclip" />
+      <Dropdown iconName="fa-ellipsis-v">
+        <span>Contact Info</span>
+        <span>Clear Messages</span>
+        <span>Delete Chat</span>
+      </Dropdown>
+    </div>
+  </header>
 );
 
 export default header;
--- src/components/Sidebar/Header/Header.jsx
+++ src/components/Sidebar/Header/Header.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
+import Dropdown from '../../Dropdown/Dropdown';
+
 import pp from '../../../assets/pp.jpg';
 
 import './Header.scss';
 
 const sidebarHeader = () => (
-  <header className="sidebar-header">
+   <header className="sidebar-header">
     <img className='thumbnail' src={pp} />
 
     <div>
-      <i className="fa fa-envelope" />
-      <i className="fa fa-ellipsis-h" />
+      {/* <i className="fa fa-envelope" /> */}
+      <Dropdown
+        iconName='fa-ellipsis-v'>
+        <span>Profile</span>
+        <span>Settings</span>
+        <span>Logout</span>
+      </Dropdown>
     </div>
   </header>
 );
