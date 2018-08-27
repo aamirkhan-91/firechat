c3ec02b852af5b20ec5918fa884646ccbc70f377
Aamir Khan <aamir.fastnu@gmail.com>
2018-08-28 01:25:20 +0500

    Removed firebase information

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var config = {
-  apiKey: "AIzaSyDBlBqNxlUM7UzgqctMt8Y1P8UnKlNQeYo",
-  authDomain: "chat-app-af727.firebaseapp.com",
-  databaseURL: "https://chat-app-af727.firebaseio.com",
-  projectId: "chat-app-af727",
-  storageBucket: "chat-app-af727.appspot.com",
-  messagingSenderId: "974255361575"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 const firebaseApp = firebase.initializeApp(config);
