65a1c7ce0b3e84b56017c257fac324780a393275
Aamir Khan <aamir.fastnu@gmail.com>
2018-08-28 01:02:21 +0500

    Created a Loader component

---
+++ src/utilities/Loader/Loader.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { CSSTransition } from "react-transition-group";
+
+import "./Loader.scss";
+
+const Loader = props => {
+  let component;
+  let classes = ["loader"];
+
+  if (props.overlay) {
+    classes.push("overlay");
+  }
+
+  if (props.transition) {
+    component = (
+      <CSSTransition classNames="loader" unmountOnExit appear in={props.show} timeout={350}>
+        <div className={classes.join(" ")}>
+          <i className="fa fa-spinner fa-spin fa-3x" />
+        </div>
+      </CSSTransition>
+    );
+  } else {
+    if (!props.show) {
+      return null;
+    }
+
+    component = (
+      <div className={classes.join(" ")}>
+        <i className="fa fa-spinner fa-spin fa-3x" />
+      </div>
+    );
+  }
+
+  return component;
+};
+
+export default Loader;
