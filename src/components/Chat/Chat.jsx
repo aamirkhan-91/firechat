49d268386f7fe57f0d63c67d9ef3e7c6f7ed1e39
Aamir Khan <aamir.fastnu@gmail.com>
2018-08-28 22:12:21 +0500

    Added differenciation for sender/recipient in chat view

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -11,7 +11,7 @@ const chat = props => (
   <div className="chat">
     <Header contact={props.selectedContact} />
     <div className="chat__messages">
-      { props.messages.length ? props.messages.map(message => (<Message message={message.message} />)) : null }
+      { props.messages.length ? props.messages.map(message => (<Message currentUser={props.currentUser} message={message} />)) : null }
     </div>
     <Input send={props.onSendMessage} />
   </div>
--- src/components/Chat/Message/Message.jsx
+++ src/components/Chat/Message/Message.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import './Message.scss';
 
 const message = props => (
-  <div className='message'>
-    {props.message}
+  <div className={'message ' + (props.currentUser.uid === props.message.sender ? 'sender' : null)}>
+    {props.message.message}
   </div>
 )
 
