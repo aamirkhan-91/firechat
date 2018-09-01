80ca2387fb55f62c759d53664e3cac431df80f64
Aamir Khan <aamir.fastnu@gmail.com>
2018-09-01 13:53:18 +0500

    Improved Auth routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, withRouter } from "react-router-dom";
 
 import Layout from "./containers/Layout/Layout";
 import Auth from "./containers/Auth/Auth";
@@ -44,10 +44,17 @@ class App extends Component {
 
   render() {
     let authenticated = this.state.user ? true : false;
+    let authRedirect = '/auth';
+
+    if (!authenticated) {
+      if (this.props.location.pathname.includes('auth')) {
+        authRedirect = this.props.location.pathname;
+      }
+    }
+
     let content = !this.state.initialLoad ? (
     <Aux>
-      {authenticated ? <Redirect to="/app" /> : <Redirect to="/auth" />}
-
+      {authenticated ? <Redirect to="/app" /> : <Redirect to={authRedirect} />}
 
       <Route path="/app" component={Layout} />
       <Route path="/auth" component={Auth} />
@@ -63,4 +70,4 @@ class App extends Component {
 }
 
 /* eslint-disable */
-export default hot(module)(App);
+export default hot(module)(withRouter(App));
