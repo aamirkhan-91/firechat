25672dee52e5587a6e4e65ad13b89a5b9e420f3e
Aamir Khan <aamir.fastnu@gmail.com>
2018-09-03 02:20:31 +0500

    Added message time stamps and split messages by dates

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -5,16 +5,44 @@ import Input from "./Input/Input";
 
 import Message from './Message/Message';
 
+import Aux from '@/utilities/Auxillary';
+
+import moment from 'moment';
+
 import "./Chat.scss";
 
-const chat = props => (
-  <div className="chat">
-    <Header contact={props.selectedContact} />
-    <div className="chat__messages">
-      { props.messages.length ? props.messages.map(message => (<Message currentUser={props.currentUser} message={message} />)) : null }
+const chat = props => {
+
+  let currentDate;
+  if (props.messages.length) {
+    currentDate = moment(props.messages[0].timestamp).format('DD/MM/YYYY');
+    debugger;
+  }
+
+  return (
+    <div className="chat">
+      <Header contact={props.selectedContact} />
+      <div className="chat__messages">
+        <div className="chat__messages__date">{currentDate}</div>
+        { props.messages.length ? props.messages.map(message => {
+          let date = moment(message.timestamp).format('DD/MM/YYYY');
+
+          if (date !== currentDate) {
+            currentDate = date;
+            return (
+              <Aux>
+                <div className="chat__messages__date">{currentDate}</div>
+                <Message currentUser={props.currentUser} message={message} />
+              </Aux>
+            );
+          } else {
+            return <Message currentUser={props.currentUser} message={message} />
+          }
+        }) : null }
+      </div>
+      <Input send={props.onSendMessage} />
     </div>
-    <Input send={props.onSendMessage} />
-  </div>
-);
+  );
+};
 
 export default chat;
--- src/components/Chat/Message/Message.jsx
+++ src/components/Chat/Message/Message.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import moment from 'moment';
+
 import './Message.scss';
 
 const message = props => (
   <div className={'message ' + (props.currentUser.uid === props.message.sender ? 'sender' : null)}>
     {props.message.message}
+    <span className='message__timestamp'> {moment(props.message.timestamp).format('hh:mm A')} </span>
   </div>
 )
 
