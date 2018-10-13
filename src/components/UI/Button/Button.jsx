7111dfdcb25ff06ac90189bb5a5e9f1e311cb262
Aamir Khan <aamir.fastnu@gmail.com>
2018-10-13 20:53:47 +0500

    Added additional customizations to <Button />

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -3,11 +3,25 @@ import React from "react";
 import "./Button.scss";
 
 const button = props => {
+  let classes = ['btn', 'primary'];
+
+  if (props.accent) {
+    classes.push('accent');
+  }
+
+  if (props.marginBottom) {
+    classes.push('margin-bottom');
+  }
+
+  if (props.hasLabel) {
+    classes.push('has-label');
+  }
+
   return (
     <button
       onClick={props.clicked}
       disabled={props.disabled}
-      className="btn btn-primary"
+      className={classes.join(" ")}
     >
       {props.text}
     </button>
