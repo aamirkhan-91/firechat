984b19ac9bcab22c8e636ea9ee4bd0c2f259f44a
Aamir Khan <aamir.fastnu@gmail.com>
2018-10-17 13:51:56 +0500

    Removed Auth route transition

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,43 +1,30 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 
-import posed, { PoseGroup } from 'react-pose';
-
 import Signup from "./Signup";
 import Signin from "./Signin";
 
 import "./Auth.scss";
 
-const RouteContainer = posed.div({
-  enter: { opacity: 1, delay: 300, transition: { duration: 250 } },
-  exit: { opacity: 0, transition: { duration: 250 } }
-});
-
 const auth = props => (
   <div className="auth">
-    <Route render={({ location }) => (
-      <PoseGroup>
-        <RouteContainer key={location.key} >
-          <Switch location={location}>
-            <Route
-              exact
-              path={props.match.url + "/signup"}
-              component={Signup}
-            />
-            <Route
-              exact
-              path={props.match.url + "/signin"}
-              component={Signin}
-            />
-            <Route
-              exact
-              path={props.match.url}
-              render={() => <Redirect to={props.match.url + "/signup"} />}
-            />
-          </Switch>
-        </RouteContainer>
-      </PoseGroup>
-    )} />
+    <Switch location={location}>
+      <Route
+        exact
+        path={props.match.url + "/signup"}
+        component={Signup}
+      />
+      <Route
+        exact
+        path={props.match.url + "/signin"}
+        component={Signin}
+      />
+      <Route
+        exact
+        path={props.match.url}
+        render={() => <Redirect to={props.match.url + "/signup"} />}
+      />
+    </Switch>
   </div>
 );
 
