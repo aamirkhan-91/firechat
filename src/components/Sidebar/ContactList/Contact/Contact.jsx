be9b33fac862fbdbd002925d83084bd2902ec080
Aamir Khan <aamir.fastnu@gmail.com>
2018-10-31 10:18:14 +0500

    Added a timestamp to Contact in ContactList

--- src/components/Sidebar/ContactList/Contact/Contact.jsx
+++ src/components/Sidebar/ContactList/Contact/Contact.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import pp from "../../../../assets/pp.png";
 
+import * as moment from 'moment';
+
 import "./Contact.scss";
 
 const contact = props => {
@@ -12,6 +14,7 @@ const contact = props => {
   }
 
   let lastMessage = props.contact.chat ? (props.contact.chat.messages.length ? props.contact.chat.messages[props.contact.chat.messages.length - 1].message : 'Tap to start a conversation') : 'Tap to start a conversation';
+  let timeStamp = props.contact.chat ? (props.contact.chat.messages.length ? moment(props.contact.chat.messages[props.contact.chat.messages.length - 1].timestamp).format('hh:mm A') : null) : null;
 
   return (
     <div onClick={props.clicked} className="contact">
@@ -20,8 +23,9 @@ const contact = props => {
         <h3>{props.contact.fullName}</h3>
         <p> { lastMessage } </p>
 
-        {/* <i className='fa fa-caret-right'></i> */}
       </div>
+      {/* <i className='contact__chevron fa fa-chevron-down'></i> */}
+      <span className="contact__timestamp">{timeStamp}</span>
     </div>
   );
 };
